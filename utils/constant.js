a6edaba9e1509682b4c7fcc04cfe9eaea1325519
rohitrikhi <rohitrikhibtc@gmail.com>
2024-12-12 17:17:09 +0530

    landing page msg updated

--- utils/constant.js
+++ utils/constant.js
@@ -1,22 +1,22 @@
 export const LABELS = {
   LANDIGN_MSG:
-    "NetQwix uses the best of technology and human expertise to deliver the ultimate learning experience to enthusiasts worldwide",
-  PROMO_ONE: "Meet Real Experts LIVE on NetQwix",
-  PROMO_TWO: "NetQwix - Live lessons with Real Experts",
+    'NetQwix uses tech & human expertise to deliver the ultimate LIVE learning experience to enthusiasts worldwide',
+  PROMO_ONE: 'Meet Real Experts LIVE on NetQwix',
+  PROMO_TWO: 'NetQwix - Live lessons with Real Experts',
   PROMO_THREE:
-    "NetQwix Instant Lessons - click here to see if your favorite expert is available right now",
+    'NetQwix Instant Lessons - click here to see if your favorite expert is available right now',
 };
 
 // export const awsS3Url = "https://netquix.s3.ap-south-1.amazonaws.com"
-export const awsS3Url = "https://data.netqwix.com/";
+export const awsS3Url = 'https://data.netqwix.com/';
 
-export const MY_CLIPS_LABEL_LIMIT= 10
+export const MY_CLIPS_LABEL_LIMIT = 10;
 
 export const notificiationTitles = {
   sessionStrated: 'Session Started',
   sessionCancelattion: 'Session Cancellation',
-  feedBackReceived:"Feedback Received",
-  sessionConfirmation: "Session Confirmation",
-  newBookingRequest:'New Booking Request',
-  gamePlanReport:'Game Plan Report'
-}
+  feedBackReceived: 'Feedback Received',
+  sessionConfirmation: 'Session Confirmation',
+  newBookingRequest: 'New Booking Request',
+  gamePlanReport: 'Game Plan Report',
+};
