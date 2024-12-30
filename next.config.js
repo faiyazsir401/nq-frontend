18593d9d0a666e85ad7487641acbb60d4f195c3a
Skysparko <shubhamrakhecha5@gmail.com>
2024-12-30 14:32:13 +0530

    added the privacy policy and t&c files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //   images: {
-    //     unoptimized: true
-    //   }
     webpack(config) {
-        config.experiments = { ...config.experiments, asyncWebAssembly: true };
-        return config;
+      config.experiments = { 
+        ...config.experiments, 
+        asyncWebAssembly: true 
+      };
+      return config;
     },
-}
-
-module.exports = nextConfig
+  
+    async rewrites() {
+      return [
+        {
+          source: '/privacy-policy',
+          destination: '/privacy-policy.html',
+        },
+        {
+            source: '/t&c',
+            destination: '/t&c.html',
+          },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
